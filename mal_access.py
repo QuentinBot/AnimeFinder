c068433721d5c068a6c7990aa14f5c3fe34a49ea
Quentin <quentin.muench@web.de>
2025-04-23 14:21:27 +0200

    added api request retries and exception block

--- mal_access.py
+++ mal_access.py
@@ -1,9 +1,12 @@
 import requests
+import sys
+
 from config import MAL_KEY
+
 
 auth = {"X-MAL-CLIENT-ID": MAL_KEY}
 
-# TODO: Handle exceptions of requests module
+
 def get_seasonal_anime(year, season, limit=100):
     params = {
         "nsfw": "true",
@@ -11,8 +14,8 @@ def get_seasonal_anime(year, season, limit=100):
         "limit": limit,
         "fields": "id,title,num_list_users,media_type"
     }
-    response = requests.get(f'https://api.myanimelist.net/v2/anime/season/{year}/{season}', headers=auth, params=params)
-    return response.json()
+    response = get_api_response(f"season/{year}/{season}", params=params)
+    return response
 
 
 def get_upcoming_anime(limit=50):
@@ -21,13 +24,26 @@ def get_upcoming_anime(limit=50):
         "limit": limit,
         "fields": "id,title,num_list_users,media_type,related_anime"
     }
-    response = requests.get('https://api.myanimelist.net/v2/anime/ranking', headers=auth, params=params)
-    return response.json()
+    response = get_api_response("ranking", params=params)
+    return response
 
 
 def get_anime_details(anime_id):
     params = {
         "fields": "related_anime"
     }
-    response = requests.get(f'https://api.myanimelist.net/v2/anime/{anime_id}', headers=auth, params=params)
-    return response.json()
+    response = get_api_response(anime_id, params=params)
+    return response
+
+
+def get_api_response(endpoint, params=None):
+    for _ in range(3):  # Retry up to 3 times
+        try:
+            response = requests.get(f'https://api.myanimelist.net/v2/anime/{endpoint}', headers=auth, params=params)
+            return response.json()
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            print("Retrying...")
+            sys.sleep(5)
+    
+    return None  
