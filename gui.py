c3140649f9e7d27c90592fc96a1a636a74098a84
Quentin <quentin.muench@web.de>
2025-04-23 15:33:15 +0200

    reorganised files

--- gui.py
+++ gui.py
@@ -1,113 +1,239 @@
-import main
+import ttkbootstrap as ttk
+from ttkbootstrap.scrolled import ScrolledFrame
 import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
+import datetime
+from PIL import Image, ImageTk
 
-def find_sequel_seasonal_gui():
-    mediatype_colors = {
-        "MOVIE": "purple",
-        "TV": "blue",
-        "OVA": "olive",
-        "ONA": "green",
-        "SPECIAL": "brown",
-        "MUSIC": "gray",
-        "UNKNOWN": "black",
-        "TV_SPECIAL": "red"
-    }
-    background_colors = ["white", "#ccffcc", "#ffcccc"] # "#ccffcc" = green, "#ffcccc" = red
+import mal_access
+import util
+from util import SEASONS, BACKGROUND_COLORS, CURRENT_SEASON_THRESHOLD, MEDIA_TYPE_TO_COLOR
 
-    def find_sequel_seasonal():
-        year = year_entry.get()
-        season = season_var.get()
-        result = main.find_sequel_seasonal(year, season)
-        result.sort(key=lambda x: x.split(":")[0])  # Sort the result by mediatype
-        result_text.delete(1.0, tk.END)  # Clear the text widget
 
-        filename = f"./Savestates/sequel_seasonal_{year}_{season}.txt"
-        background_dictionary = {}
-        try:
-            with open(filename, "r") as file:
-                saved_lines = file.readlines()
-                for line in saved_lines:
-                    color, tag = line.split()
-                    background_dictionary[tag] = color
-        except FileNotFoundError:
-            pass
+def change_anime_status(label, direction):
+    color = str(label.cget("background"))
+    index = BACKGROUND_COLORS.index(color)
+    new_index = (index + direction) % len(BACKGROUND_COLORS)
+    label.config(background=BACKGROUND_COLORS[new_index])
+    for widget in label.winfo_children():
+        widget.config(background=BACKGROUND_COLORS[new_index])
 
-        for i in result:
-            media_type = i.split(":")[0]
-            mal_id = i.split(" - ")[-1].split("/")[-1]
-            if mal_id not in background_dictionary:
-                background_dictionary[mal_id] = "white"
 
-            result_text.tag_configure(mal_id, background=background_dictionary[mal_id], foreground=mediatype_colors[media_type])  # Create a tag for this media type
-            result_text.insert(tk.END, f"{i}\n", mal_id)  # Apply the tag to the inserted text
-            result_text.tag_bind(mal_id, "<Button-1>", lambda e, tag=mal_id: change_color(tag, 1))  # Bind left click event to change color
-            result_text.tag_bind(mal_id, "<Button-3>", lambda e, tag=mal_id: change_color(tag, -1))  # Bind right click event to change color
-
-    def change_color(tag, direction):
-        # Change the color of the line when clicked
-        current_color = result_text.tag_cget(tag, "background")
-        new_color = background_colors[(background_colors.index(current_color) + direction) % len(background_colors)]
-        result_text.tag_configure(tag, background=new_color)
-        
-    def save_state():
-        year = year_entry.get()
-        season = season_var.get()
-        filename = f"sequel_seasonal_{year}_{season}.txt"
-        with open("./Savestates/" + filename, "w") as file:
-            for line in result_text.get(1.0, tk.END).splitlines():
-                tag = line.split("/")[-1]
-                if tag:
-                    current_color = result_text.tag_cget(tag, "background")      
-                    file.write(f"{current_color} {tag}\n")
-        messagebox.showinfo("Save State", f"Results saved to {filename}")
-
-    # Create the main window
-    window = tk.Tk()
-    window.title("Find Sequel Seasonal")
+def refresh_frame_data(frame, retrieved_data, saved_data, user_threshold=0):
+    for widget in frame.winfo_children():
+        widget.destroy()
     
-    # Create a label
-    label = ttk.Label(window, text="Find Sequel Seasonal")
-    label.pack()
+    for anime in retrieved_data:
+        anime_id = str(anime['node']['id'])
 
-    # Create a frame for input fields
-    input_frame = ttk.Frame(window)
-    input_frame.pack()
+        if anime["node"]["num_list_users"] < user_threshold:
+            break
 
-    # Create a label and entry for year
-    year_label = ttk.Label(input_frame, text="Year:")
-    year_label.grid(row=0, column=0, padx=5, pady=5)
-    year_entry = ttk.Entry(input_frame)
-    year_entry.grid(row=0, column=1, padx=5, pady=5)
-    year_entry.insert(0, "2025")  # Autofill with year 2025
+        if anime_id not in saved_data:
+            saved_data[anime_id] = {"title": anime["node"]["title"], "num_list_users": anime["node"]["num_list_users"], "status": 0, "media_type": anime["node"]["media_type"]}
 
-    # Create a label and option menu for season name
-    season_label = ttk.Label(input_frame, text="Season Name:")
-    season_label.grid(row=1, column=0, padx=5, pady=5)
-    season_var = tk.StringVar()
-    season_option_menu = ttk.OptionMenu(input_frame, season_var, "winter", "summer", "fall", "winter", "spring")
-    season_option_menu.grid(row=1, column=1, padx=5, pady=5)
+        generate_row_frame_contents(frame, anime_id, saved_data[anime_id])
+
+
+def show_seasonal_anime(year, season, frame):
+    retrieved_seasonal_anime_data = mal_access.get_seasonal_anime(year, season)
+    filtered_seasonal_anime_data = util.filter_sequels(retrieved_seasonal_anime_data)
+    saved_seasonal_data = util.load_save_data(f"{year}_{season}")
     
-    # Create a button    
-    button = ttk.Button(window, text="Find Sequel Seasonal", command=lambda: [find_sequel_seasonal(), show_save_button()])
-    button.pack()
+    refresh_frame_data(frame, filtered_seasonal_anime_data, saved_seasonal_data)
 
-    # Create a save button but don't pack it yet
-    save_button = ttk.Button(window, text="Save", command=save_state, width=20)
-    def show_save_button():
-        save_button.pack(side=tk.BOTTOM)
-        save_button.config(style="Green.TButton")
 
-    # Create a style for the green button
-    style = ttk.Style()
-    style.configure("Green.TButton", background="green", foreground="black")
+def show_upcoming_anime(frame):
+    retrieved_upcoming_anime_data = mal_access.get_upcoming_anime()
+    filtered_upcoming_anime_data = util.filter_sequels(retrieved_upcoming_anime_data)
+    saved_upcoming_data = util.load_save_data("upcoming")
 
-    # Create a text widget to display the results
-    result_text = tk.Text(window, height=25, width=150)
-    result_text.pack()
+    refresh_frame_data(frame, filtered_upcoming_anime_data, saved_upcoming_data)
 
-    # Start the main loop
-    window.mainloop()
 
-find_sequel_seasonal_gui()
+def show_current_season_anime(frame):
+    retrieved_current_season_anime_data = mal_access.get_seasonal_anime(datetime.datetime.now().year, util.get_current_season().lower())
+    saved_current_season_data = util.load_save_data("current_season")
+
+    refresh_frame_data(frame, retrieved_current_season_anime_data["data"], saved_current_season_data, CURRENT_SEASON_THRESHOLD)
+
+
+def show_initial_anime_data(path, frame):
+    stored_data = util.load_save_data(path)
+
+    for widget in frame.winfo_children():
+        widget.destroy()
+    
+    for anime_id, data in stored_data.items():
+        generate_row_frame_contents(frame, anime_id, data)
+
+
+def initialise_scrollbar_header(frame):
+    new_frame = tk.Frame(frame)
+    new_frame.pack(fill="x")
+
+    new_frame.columnconfigure(0, weight=1)
+    new_frame.columnconfigure(1, weight=0)
+    new_frame.columnconfigure(2, weight=0)
+
+    title_label = ttk.Label(new_frame, text="Title", anchor="w")
+    title_label.grid(row=0, column=0, sticky="w", padx=7)
+    id_label = ttk.Label(new_frame, text="ID", width=6, anchor="center")
+    id_label.grid(row=0, column=1, sticky="ns")
+    num_list_users_label = ttk.Label(new_frame, text="Users", width=7, anchor="center")
+    num_list_users_label.grid(row=0, column=2, padx=(0, 17), sticky="ns")
+    horizontal_separator = ttk.Separator(frame, orient="horizontal")
+    horizontal_separator.pack(fill="x")
+
+
+def generate_row_frame_contents(frame, anime_id, data):
+    new_frame = tk.Frame(frame)
+    new_frame.bind("<Button-1>", lambda event, label=new_frame: change_anime_status(label, 1))
+    new_frame.bind("<Button-3>", lambda event, label=new_frame: change_anime_status(label, -1))
+    new_frame.pack(fill="x")
+
+    new_frame.configure(background=BACKGROUND_COLORS[data["status"]])
+
+    new_frame.columnconfigure(0, weight=1)
+    new_frame.columnconfigure(1, weight=0)
+    new_frame.columnconfigure(2, weight=0)
+
+    title_label = ttk.Label(new_frame, text=data["title"], background=BACKGROUND_COLORS[data["status"]], foreground=MEDIA_TYPE_TO_COLOR[data["media_type"]], anchor="w")
+    title_label.bind("<Button-1>", lambda event, label=title_label: change_anime_status(label.master, 1))
+    title_label.bind("<Button-3>", lambda event, label=title_label: change_anime_status(label.master, -1))
+    title_label.grid(row=0, column=0, sticky="w", padx=5)
+    id_label = ttk.Label(new_frame, text=anime_id, background=BACKGROUND_COLORS[data["status"]], anchor="center", width=6)
+    id_label.bind("<Button-1>", lambda event, label=id_label: change_anime_status(label.master, 1))
+    id_label.bind("<Button-3>", lambda event, label=id_label: change_anime_status(label.master, -1))
+    id_label.grid(row=0, column=1)
+    num_list_users_label = ttk.Label(new_frame, text=data["num_list_users"], background=BACKGROUND_COLORS[data["status"]], anchor="center", width=7)
+    num_list_users_label.bind("<Button-1>", lambda event, label=num_list_users_label: change_anime_status(label.master, 1))
+    num_list_users_label.bind("<Button-3>", lambda event, label=num_list_users_label: change_anime_status(label.master, -1))
+    num_list_users_label.grid(row=0, column=2, padx=(0, 15))
+
+
+def insert_background_image(window, image_path, width, height, x, y, scale):
+    original_image = Image.open(image_path)
+    resized_image = original_image.resize((int(width * scale), int(height * scale)))
+    background_image = ImageTk.PhotoImage(resized_image)
+    background_label = tk.Label(window, image=background_image)
+    background_label.place(x=x, y=y, anchor="nw")
+    background_label.image = background_image  # Keep a reference to avoid garbage collection
+
+
+# TODO: Update Readme
+def gui():
+
+    root = ttk.Window(title="Anime Recommender", themename="flatly", size=(1400, 700), resizable=(False, False))
+    root.position_center()
+
+    insert_background_image(root, "assets/naruto.png", 920, 832, 50, 10, 0.15)
+    insert_background_image(root, "assets/rengoku.png", 646, 690, 1175, 20, 0.22)
+    insert_background_image(root, "assets/frieren.png", 595, 630, 300, 5, 0.2)
+    insert_background_image(root, "assets/itadori.png", 543, 775, 1000, 10, 0.2)
+ 
+    year_valid_function = root.register(util.validate_year)
+
+    initial_label = ttk.Label(root, text="Anime Recommender", font=("Helvetica", 32))
+    initial_label.pack(pady=5)
+
+    canvas = tk.Canvas(root, width=335, height=50, highlightthickness=0)
+    canvas.pack(pady=2, side="top")
+    canvas.create_rectangle(5, 5, 330, 45, outline="black", dash=(3, 5))
+    legend_frame = tk.Frame(canvas)
+    legend_frame.place(relx=0.5, rely=0.5, anchor="center")
+    legend_label = ttk.Label(legend_frame, text="Legend", font=("Helvetica", 10, "bold"))
+    legend_label.pack()
+    for media_type, color in MEDIA_TYPE_TO_COLOR.items():
+        media_type_label = ttk.Label(legend_frame, text=media_type.upper(), foreground=color, anchor="w", font=("Helvetica", 8))
+        media_type_label.pack(side="left", fill="x", expand=True)
+
+    # Seasonal Anime
+    seasonal_root = tk.Frame(root, highlightbackground="black", highlightthickness=1)
+    seasonal_root.pack(pady=5, expand=True, side="left")   
+
+    seasonal_label = ttk.Label(seasonal_root, text="Seasonal Anime", font=("Helvetica", 16))
+    seasonal_label.pack(pady=10)
+
+    year_frame = ttk.Frame(seasonal_root)
+    year_frame.pack(pady=5)
+    year_label = ttk.Label(year_frame, text="Enter Year:")
+    year_label.pack(side="left", expand=True, padx=5)
+    year_entry = ttk.Spinbox(year_frame, from_=1916, to=datetime.datetime.now().year + 3, increment=1, width=5, validate="focus", validatecommand=(year_valid_function, "%P"), wrap=True, command=lambda: show_initial_anime_data(f"{year_entry.get()}_{season_var.get().lower()}", sequels_frame))
+    year_entry.set(datetime.datetime.now().year)
+    year_entry.pack(side="left", expand=True, padx=5)
+
+    season_frame = ttk.Frame(seasonal_root)
+    season_frame.pack(pady=5)
+    season_label = ttk.Label(season_frame, text="Select Season:")
+    season_label.pack(side="left", expand=True, padx=5)
+    season_var = ttk.StringVar()
+    season_menu = ttk.OptionMenu(season_frame, season_var, util.get_next_season(), *SEASONS, command=lambda val: show_initial_anime_data(f"{year_entry.get()}_{season_var.get().lower()}", sequels_frame))
+    season_menu.pack(side="left", expand=True, padx=5)
+
+    search_season_button = ttk.Button(seasonal_root, text="Search Selected Season", command=lambda: show_seasonal_anime(year_entry.get(), season_var.get().lower(), sequels_frame))
+    search_season_button.pack(pady=5)
+
+    sequels_border_frame = tk.Frame(seasonal_root, highlightbackground="black", highlightthickness=1, width=400, height=300)
+    sequels_border_frame.pack(pady=5, padx=5)
+    initialise_scrollbar_header(sequels_border_frame)
+
+    sequels_frame = ScrolledFrame(sequels_border_frame, width=400, height=300, autohide=True)
+    sequels_frame.pack()
+
+    save_seasonal_button = ttk.Button(seasonal_root, text="Save Changes", command=lambda: util.save_changes(f"{year_entry.get()}_{season_var.get().lower()}", sequels_frame))
+    save_seasonal_button.pack(pady=5)
+
+    show_initial_anime_data(f"{year_entry.get()}_{season_var.get().lower()}", sequels_frame)
+
+
+    # Current Season Anime
+    current_season_root = tk.Frame(root, highlightbackground="black", highlightthickness=1)
+    current_season_root.pack(pady=5, expand=True, side="left")
+
+    current_season_label = ttk.Label(current_season_root, text="Current Season Anime", font=("Helvetica", 16))
+    current_season_label.pack(pady=10)
+
+    current_season_button = ttk.Button(current_season_root, text="Search Current Season", command=lambda: show_current_season_anime(current_season_frame))
+    current_season_button.pack(pady=5)
+
+    current_season_border_frame = tk.Frame(current_season_root, highlightbackground="black", highlightthickness=1, width=400, height=300)
+    current_season_border_frame.pack(pady=5, padx=5)
+    initialise_scrollbar_header(current_season_border_frame)
+
+    current_season_frame = ScrolledFrame(current_season_border_frame, width=400, height=300, autohide=True)
+    current_season_frame.pack()
+
+    save_current_season_button = ttk.Button(current_season_root, text="Save Changes", command=lambda: util.save_changes("current_season", current_season_frame))
+    save_current_season_button.pack(pady=5)
+
+    show_initial_anime_data("current_season", current_season_frame)
+
+
+    # Upcoming Anime
+    upcoming_root = tk.Frame(root, highlightbackground="black", highlightthickness=1)
+    upcoming_root.pack(pady=5, expand=True, side="left") 
+
+    upcoming_label = ttk.Label(upcoming_root, text="Upcoming Anime", font=("Helvetica", 16))
+    upcoming_label.pack(pady=10)
+
+    search_upcoming_button = ttk.Button(upcoming_root, text="Search Upcoming Anime", command=lambda: show_upcoming_anime(upcoming_frame))
+    search_upcoming_button.pack(pady=5)
+
+    upcoming_border_frame = tk.Frame(upcoming_root, highlightbackground="black", highlightthickness=1, width=400, height=300)
+    upcoming_border_frame.pack(pady=5, padx=5)
+    
+    initialise_scrollbar_header(upcoming_border_frame)
+
+    upcoming_frame = ScrolledFrame(upcoming_border_frame, width=400, height=300, autohide=True)
+    upcoming_frame.pack()
+
+    save_upcoming_button = ttk.Button(upcoming_root, text="Save Changes", command=lambda: util.save_changes("upcoming", upcoming_frame))
+    save_upcoming_button.pack(pady=5)
+
+    show_initial_anime_data("upcoming", upcoming_frame)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    gui()
---
+++ legacy/gui.py
@@ -0,0 +1,113 @@
+import main
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+def find_sequel_seasonal_gui():
+    mediatype_colors = {
+        "MOVIE": "purple",
+        "TV": "blue",
+        "OVA": "olive",
+        "ONA": "green",
+        "SPECIAL": "brown",
+        "MUSIC": "gray",
+        "UNKNOWN": "black",
+        "TV_SPECIAL": "red"
+    }
+    background_colors = ["white", "#ccffcc", "#ffcccc"] # "#ccffcc" = green, "#ffcccc" = red
+
+    def find_sequel_seasonal():
+        year = year_entry.get()
+        season = season_var.get()
+        result = main.find_sequel_seasonal(year, season)
+        result.sort(key=lambda x: x.split(":")[0])  # Sort the result by mediatype
+        result_text.delete(1.0, tk.END)  # Clear the text widget
+
+        filename = f"./Savestates/sequel_seasonal_{year}_{season}.txt"
+        background_dictionary = {}
+        try:
+            with open(filename, "r") as file:
+                saved_lines = file.readlines()
+                for line in saved_lines:
+                    color, tag = line.split()
+                    background_dictionary[tag] = color
+        except FileNotFoundError:
+            pass
+
+        for i in result:
+            media_type = i.split(":")[0]
+            mal_id = i.split(" - ")[-1].split("/")[-1]
+            if mal_id not in background_dictionary:
+                background_dictionary[mal_id] = "white"
+
+            result_text.tag_configure(mal_id, background=background_dictionary[mal_id], foreground=mediatype_colors[media_type])  # Create a tag for this media type
+            result_text.insert(tk.END, f"{i}\n", mal_id)  # Apply the tag to the inserted text
+            result_text.tag_bind(mal_id, "<Button-1>", lambda e, tag=mal_id: change_color(tag, 1))  # Bind left click event to change color
+            result_text.tag_bind(mal_id, "<Button-3>", lambda e, tag=mal_id: change_color(tag, -1))  # Bind right click event to change color
+
+    def change_color(tag, direction):
+        # Change the color of the line when clicked
+        current_color = result_text.tag_cget(tag, "background")
+        new_color = background_colors[(background_colors.index(current_color) + direction) % len(background_colors)]
+        result_text.tag_configure(tag, background=new_color)
+        
+    def save_state():
+        year = year_entry.get()
+        season = season_var.get()
+        filename = f"sequel_seasonal_{year}_{season}.txt"
+        with open("./Savestates/" + filename, "w") as file:
+            for line in result_text.get(1.0, tk.END).splitlines():
+                tag = line.split("/")[-1]
+                if tag:
+                    current_color = result_text.tag_cget(tag, "background")      
+                    file.write(f"{current_color} {tag}\n")
+        messagebox.showinfo("Save State", f"Results saved to {filename}")
+
+    # Create the main window
+    window = tk.Tk()
+    window.title("Find Sequel Seasonal")
+    
+    # Create a label
+    label = ttk.Label(window, text="Find Sequel Seasonal")
+    label.pack()
+
+    # Create a frame for input fields
+    input_frame = ttk.Frame(window)
+    input_frame.pack()
+
+    # Create a label and entry for year
+    year_label = ttk.Label(input_frame, text="Year:")
+    year_label.grid(row=0, column=0, padx=5, pady=5)
+    year_entry = ttk.Entry(input_frame)
+    year_entry.grid(row=0, column=1, padx=5, pady=5)
+    year_entry.insert(0, "2025")  # Autofill with year 2025
+
+    # Create a label and option menu for season name
+    season_label = ttk.Label(input_frame, text="Season Name:")
+    season_label.grid(row=1, column=0, padx=5, pady=5)
+    season_var = tk.StringVar()
+    season_option_menu = ttk.OptionMenu(input_frame, season_var, "winter", "summer", "fall", "winter", "spring")
+    season_option_menu.grid(row=1, column=1, padx=5, pady=5)
+    
+    # Create a button    
+    button = ttk.Button(window, text="Find Sequel Seasonal", command=lambda: [find_sequel_seasonal(), show_save_button()])
+    button.pack()
+
+    # Create a save button but don't pack it yet
+    save_button = ttk.Button(window, text="Save", command=save_state, width=20)
+    def show_save_button():
+        save_button.pack(side=tk.BOTTOM)
+        save_button.config(style="Green.TButton")
+
+    # Create a style for the green button
+    style = ttk.Style()
+    style.configure("Green.TButton", background="green", foreground="black")
+
+    # Create a text widget to display the results
+    result_text = tk.Text(window, height=25, width=150)
+    result_text.pack()
+
+    # Start the main loop
+    window.mainloop()
+
+find_sequel_seasonal_gui()
